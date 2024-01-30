0dedcaf88fe8dc4a01c9c3b0bfc669a4c3023119
Mohammad Arshad <mdarshad1000@gmail.com>
2024-01-30 05:34:47 +0530

    improve prompt

--- rag.py
+++ rag.py
@@ -1,18 +1,20 @@
 from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
 from llama_index.llms import OpenAI as LlamaIndexOpenAI
 
+
 SYSTEM_PROMPT=''' 
     You are an expert in answering Research question. \
     You will receive a query or a question from the user, along with 3 articles/research paper for that query.\
-    Make use of the most apt and relevant information for answering."
+    Make use of the relevant information for answering. Do not start you answer with 'Based on the given context' or similar phrases."
     '''
 
-def rag_pipeline(u_id):
+
+def rag_pipeline(u_id, system_prompt):
     service_context = ServiceContext.from_defaults(
         llm=LlamaIndexOpenAI(
             model='gpt-3.5-turbo',
             temperature=0.5,
-            system_prompt=SYSTEM_PROMPT,
+            system_prompt=system_prompt,
         ),
         # embed_model=OpenAIEmbedding('text-embedding-3-small'),
         chunk_size=500,
