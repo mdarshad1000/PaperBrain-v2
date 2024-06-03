26b792dd63180e93d10471b446865a282ef32b1c
Mohammad Arshad <mdarshad1000@gmail.com>
2024-06-03 17:22:21 +0530

    Data folder & prompts

---
+++ config.py
@@ -0,0 +1,167 @@
+# config.py
+
+# Podcast Path
+PODCAST_STORAGE_PATH = "static/podcast/"
+
+# Audio Files Path
+MUSIC_ESSENTIALS = "static/music_essentials/"
+
+# Embedding model
+EMBEDDING_MODEL = "text-embedding-ada-002"
+
+# Prompt for Chat with Paper
+CHAT_PROMPT_TEMPLATE = """
+    You are an expert assistant for question-answering tasks. Use the following pieces of retrieved context (delimited by <ctx></ctx>)
+    to answer the question. 
+    Use four-five sentences and keep the answer concise and to the point, unless the user asks you to be detailed.
+    Be very polite and respectful. Give the answers in points whenever required. Use paragraphs and proper formatting. 
+    If not required then do not take the entire context for answering. Choose only the most relevant information from the context for answering
+    ------
+    <ctx>
+    {context}
+    </ctx>
+    ------
+    Question:
+    {question}
+    Answer:
+    """
+
+# Prompt for Podcast Retrieval
+PODCAST_PROMPT_TEMPLATE = """
+    You are an expert assistant for summarising and information extraction. 
+    Use the following pieces of retrieved content to answer the question. 
+    Be elaborate and keep the answer information rich and to the point.
+    Be very polite and respectful. Give the answers in points whenever required.
+    Use all the provided context for answering the questions.
+    If you cannot compute the answer from the context, then do not make things up
+
+    Question: {question} 
+
+    Context: {context} 
+
+    Answer:
+"""
+
+# Gemini System Instruction
+GEMINI_SYSTEM_INSTRUCTION = """
+    You are a research expert, highly skilled in extracting key information from research papers. 
+    Given a research paper, you extract the important sections and information from the paper.
+    The final report should contain headings, subheadings, and relevant content in a distilled and detailed format.
+    Make at least 5-6 topics.
+    
+    The report should contain the right amount of jargon, not too much not too less.
+    Some potential topics could be LIMITATIONS, APPLICATIONS, STRENGTHS, FUTURE DIRECTIONS, METHODS, MAIN FINDINGS. 
+    
+    IMPORTANT NOTE - You don't necessarily have to use these topics if they don't seem fit.
+"""
+
+# Gemini User Instruction
+GEMINI_USER_INSTRUCTION = """
+    Given below is a Research Paper. Generate a highly detailed, long format report of the research paper.
+    Contain a good mix of stats, jargon, layman explanation etc. Be to the point and 
+    touch up on all the aspects of the paper.
+    OUTPUT FORMAT:
+
+    1:  TOPIC 1
+        - SubTopic 1
+        <<Information about subtopic 1>>
+        - SubTopic 2
+        <<Information about subtopic 2>>
+        - SubTopic3
+        <<Information about subtopic 3>>
+    
+    
+    2:  TOPIC 2
+        - SubTopic 1
+        <<Information about subtopic 1>>
+        - SubTopic 2
+        <<Information about subtopic 2>>
+        - SubTopic3
+        <<Information about subtopic 3>>
+
+        ......
+    
+    N:  TOPIC N
+    - SubTopic 1
+    <<Information about subtopic 1>>
+    - SubTopic 2
+    <<Information about subtopic 2>>
+    - SubTopic3
+    <<Information about subtopic 3>>
+
+    RESEARCH PAPER:
+
+    {research_paper_text}
+"""
+
+# GPT System Prompt
+GPT_SYSTEM_PROMPT = """
+    You are a podcast script writer, highly skilled in generating engaging intellectual questions and answers in an
+    easy-to-understand podcast style.
+
+    Podcasters:
+        The Interviewer's name is Noah Bennett, and the expert name is Ethan Sullivan. Keep in mind that Ethan is not a
+        co-author or anyone related to the paper, he is just an Expert. You may consult another Specialist Emma Anderson
+        in between the podcast, or even at the starting too (rarely). When consulting her, give a very quick introduction.
+        She should have a minimum 2 and maximum of 3 dialogues. Include Emma in the conversation in a very smooth and intelligent way.
+
+    Tone:
+        Maintain a conversational yet authoritative tone. The conversation should also contain short dialogues back-and-forth. Noah and Ethan should engage the audience by discussing the paper's
+        content with enthusiasm and expertise. Emma should be consulted to enlighten the conversation with her two cents .
+        Use conversational fillers like Umm, Ohhh I see, Wow, Hmm, Oh, Mmm, Oh-huh, hold on, I mean, etcetera to make the conversation more
+        natural. 
+
+    Very Important Note:
+        Provide output in valid JSON format having Keys NOAH1, NOAH2, ... NOAHn for Noah's dialogue where 1, 2, ... n are the dialogue number.
+        Similarly, the Keys for ETHAN should be ETHAN1, ETHAN2, ..., ETHANn where 1, 2, ... n are the dialogue number.
+        Lastly, the Keys for EMMA should be EMMA1, EMMA2, ... EMMAn where 1, 2, ... n are the dialogue number.
+
+    Make sure to have a very catchy introduction and a very cool sounding conclusion (farewell to expert and audience).
+    Generate 15-17 dialouges at maximum.
+
+    Additional Notes:
+        Use a blend of technical language and layman terms to make the content accessible to a wide audience.
+        Keep the discussion engaging and avoid jargon overload.
+        Ensure that each section flows naturally into the next, maintaining a coherent narrative throughout the script.
+    """
+
+# GPT User Prompt
+GPT_USER_PROMPT = """
+    Based on the given context from a research paper, generate an entire podcast script having 4-5 questions.
+    Keep the podcast engaging, ask follow up questions. You may use analogies sometimes to explain the concepts.
+    In the introduction always include the phrase 'Welcome to Paper Brain'. Introduce  NOAH, ETHAN in the beginning, and introduce EMMA when required.
+    In the conclusion always include a tweaked version of -- 'Check out PaperBrain to explore scientific literature like never before!'
+    You are also provided the metadata of the Paper i.e TITLE, AUTHORS, ABSTRACT. Make use of these information
+    in the introduction.
+    
+    CONTEXT:
+
+    """
+
+ASK_SYSTEM_PROMPT = """
+    Answer with all related pieces of knowledge. Always reference between phrases the ones you use. If you skip one, you will be penalized.
+
+    Use the format [citationId] between sentences. Use the exact same "citationId" present in the knowledge pieces.
+
+    You are an Expert in answering Research question. Your answer is to the point and you don't make things up.
+    Make use of the relevant information for answering. Do not start you answer with 'Based on the given context' or similar phrases.
+
+    Your Final answer should be in the format with following.
+        *INTRODUCTION*
+        *KEY INSIGHTS*
+        *CONCLUSION*
+
+    VERY IMPORTANT NOTE: You should use the citationId in the answer. 
+    Example:
+        The capital of Chile is Santiago de Chile[1234.56789], and the population is 7 million people[2303.00980].
+"""
+
+# ASK_SYSTEM_PROMPT="""
+#     You are an Expert in answering Research question. Your answer is to the point and you don't make things up.
+#     You will receive a query or a question from Me, along with 4 articles/research paper for that query.
+#     Make use of the relevant information for answering. Do not start you answer with 'Based on the given context' or similar phrases.
+#     ---------------------
+#     {context_str}
+#     ---------------------
+#     Given this information, please answer the question: {query_str}
+# """
---
+++ data/utils.py
@@ -0,0 +1,136 @@
+import os
+import json
+from openai import OpenAI
+import tiktoken
+from tqdm import tqdm
+from paper import Paper
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def pinecone_connect():
+    """
+    Connects to the Pinecone service, creates an index if it
+    doesn't exist, and returns the Pinecone client and index object.
+
+    Returns:
+        A tuple containing the Pinecone client and index object.
+    """
+    index_name = os.getenv("PINECONE_INDEX_NAME")
+
+    pc = Pinecone(
+      api_key=os.getenv("PINECONE_API_KEY"),
+      environment='us-west-2',
+      pool_threads=30  # find next to api key in console
+    )
+    index = pc.Index(index_name)
+    # create a serverless
+    if index_name not in str(pc.list_indexes()):
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric="cosine",
+            spec=ServerlessSpec(
+                cloud='aws',
+                region='us-west-2'))
+    else:
+        pass
+    
+    return pc, index
+
+def load_data(file_path, categories, start_year, end_year):
+    """
+    Returns a generator over the papers contained in `file_path`, belonging to
+    the categories in `categories`, and published in or after `start_year`.
+    
+    Args:
+        file_path: The path to the JSON file containing the arXiv data
+        categories: A list of category strings
+        start_year: An integer specifying the earliest year to include
+        
+    Returns:
+        A generator over the papers satisfying the criteria.
+    """
+    json_file = open(file_path, "r", encoding="utf-8")
+    papers = (Paper(json.loads(line)) for line in json_file)
+    papers = (paper for paper in papers
+              if paper.has_category(categories) and paper.has_valid_id)
+    return (paper for paper in papers if paper.year >= start_year and paper.year < end_year)
+
+
+def pinecone_embedding_count(index_name):
+    """
+    Helper function to get the total number of embeddings stored in the Pinecone
+    index with the name specified in `index_name`.
+    
+    Args:
+        index_name: The name of the Pinecone index
+        
+    Returns:
+        The total number of embeddings stored in the Pinecone index.
+    """
+    pc, index = pinecone_connect()
+    index = pc.Index(index_name)
+    return index.describe_index_stats()["total_vector_count"]
+
+def estimate_embedding_price(papers, price_per_1k):
+    """
+    Estimates the price of embedding the papers in `papers` using OpenAI's
+    tiktoken tokenizer.
+    
+    Args:
+        papers: A list of `Paper` objects
+        price_per_1k: Price per 1000 tokens
+    
+    Returns:
+        A tuple containing the estimated number of tokens and a price.
+    """
+    enc = tiktoken.get_encoding("gpt2")
+    num_tokens = 0
+    for paper in tqdm(papers):
+        num_tokens += len(enc.encode(paper.embedding_text))
+    print(num_tokens)
+    price = num_tokens / 1000 * price_per_1k
+    return num_tokens, price
+
+
+def get_embeddings(texts, model="text-embedding-ada-002"):
+    """
+    Returns a list of embeddings for each string in `texts` using the OpenAI
+    embedding model specified in `model`.
+    
+    Args:
+        texts: A list of strings to embed
+        model: The name of the OpenAI embedding model to use
+        
+    Returns:
+        A list of embeddings.
+    """
+    response = client.embeddings.create(input=texts, model=model)
+    return response.data
+
+def embed_and_upsert(papers, index_name, model, batch_size=90):
+    """
+    Embeds the embedding text of each paper in `papers` using the embedding
+    model specified in `model`. The embeddings are then upserted to the Pinecone
+    index with name `index_name` in batches of size `batch_size`.
+    
+    Args:
+        papers: The list of papers for which to embed their embedding text
+        index_name: The name of the index in which the embeddings will be upserted
+        model: The name of the OpenAI embedding model to use
+        batch_size: The batch size to use when upserting embeddings to Pinecone
+    """
+    pc, index = pinecone_connect()
+    with pc.Index(index_name,) as index:
+        for i in tqdm(range(0, len(papers), batch_size)):
+            batch = papers[i:i+batch_size]
+            texts = [paper.embedding_text for paper in batch]
+            embed_data = get_embeddings(texts, model)
+    
+            pc_data = [(p.id, e.embedding, p.metadata)
+                       for p, e in zip(batch, embed_data)]
+            index.upsert(pc_data)
