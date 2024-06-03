6441f8c3a51af01650f58a3335b58641f081dd36
Mohammad Arshad <mdarshad1000@gmail.com>
2024-06-03 18:03:45 +0530

    Update request body

--- app/api/routers/chat.py
+++ app/api/routers/chat.py
@@ -15,17 +15,13 @@ chat_router = r = APIRouter()
 
 prompt = create_prompt_template(CHAT_PROMPT_TEMPLATE)
 
-class ChatRequest(BaseModel):
-    paper_id: str = Field(..., description='Paper ID of the research paper')
-    message: str = Field(..., description='Question to ask the research paper')
-
 def get_pinecone_service():
     api_key = os.getenv('PINECONE_API_KEY_2')
     index_name = os.getenv('PINECONE_INDEX_NAME_2')
     environment = os.getenv('PINECONE_ENVIRONMENT_2')
     return PineconeService(api_key=api_key, index_name=index_name, environment=environment)
 
-@r.get('/explain-new')  # Changed from post to get
+@r.post('/explain-new')  # Changed from post to get
 async def explain_question(paper_id: str, message: str, pinecone_service=Depends(get_pinecone_service)):
 
     answer = ask_questions(
@@ -34,6 +30,4 @@ async def explain_question(paper_id: str, message: str, pinecone_service=Depends
         prompt=prompt,
         index=pinecone_service.index
     )
-    return {"answer": answer}
-
     return {"answer": answer}
