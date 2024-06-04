ed5d8a5c5cc5fd0c33158f53feb12b4cc19771fc
Mohammad Arshad <mdarshad1000@gmail.com>
2024-06-04 06:39:10 +0530

    feat: Chatbot with Memory

--- app/engine/chat_utils.py
+++ app/engine/chat_utils.py
@@ -8,6 +8,7 @@ from typing import List
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_pinecone import PineconeVectorStore
+from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
@@ -20,6 +21,9 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 def create_prompt_template(template_str):
+    return PromptTemplate(input_variables=["history", "context", "question"], template=template_str)
+
+def create_prompt_template_podcast(template_str):
     return PromptTemplate.from_template(template=template_str)
 
 def prepare_data(paperurl: str):
@@ -94,41 +98,63 @@ def embed_and_upsert(texts: List[str], metadatas: List[str], index) -> str:
     return "Embedded the Arxiv Paper"
 
 
-def ask_questions(question: str, paper_id: int, prompt: str, index):
-    '''
-    Retrieve Context and Generate Answers
-    '''
+class QASessionManager:
+    def __init__(self):
+        self.sessions = {}
 
-    text_field = "text"
-    # Create an embedding object
-    embeddings = OpenAIUtils.get_embeddings_api()
+    def get_session_key(self, userid, paper_id):
+        return f"{userid}_{paper_id}"
 
-    vectorstore = PineconeVectorStore(
-        index=index, embedding=embeddings, text_key=text_field,
-    )
+    def is_first_question(self, userid, paper_id):
+        session_key = self.get_session_key(userid, paper_id)
+        return session_key not in self.sessions
 
-    llm = OpenAIUtils.get_chat_api(
-        model_name='gpt-3.5-turbo-16k',
-        temperature=0.0,
-        streaming=True,
-        callbacks=[StreamingStdOutCallbackHandler()],
-    )
+    def get_session(self, userid, paper_id):
+        session_key = self.get_session_key(userid, paper_id)
+        return self.sessions.get(session_key)
 
-    qa_with_sources = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever(
-            search_kwargs={
-                'k': 4,
-                'filter': {'paper_id':{"$eq":paper_id}}
+    def create_session(self, userid, paper_id, index, embeddings, prompt):
+        text_field = "text"
+        vectorstore = PineconeVectorStore(
+            index=index, embedding=embeddings, text_key=text_field,
+        )
+
+        llm = OpenAIUtils.get_chat_api(
+            model_name='gpt-3.5-turbo-16k',
+            temperature=0.0,
+            streaming=True,
+            callbacks=[StreamingStdOutCallbackHandler()],
+        )
+
+        qa_with_sources = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=vectorstore.as_retriever(
+                search_kwargs={
+                    'k': 4,
+                    'filter': {'paper_id': {"$eq": paper_id}}
                 }),
-        chain_type_kwargs={
-            "prompt": prompt,
-            "verbose": True,
-        }
-    )
-    answer = qa_with_sources.run(question)
+            chain_type_kwargs={
+                "prompt": prompt,
+                "verbose": True,
+                "memory": ConversationBufferMemory(
+                    memory_key="history",
+                    input_key="question"),
+            }
+        )
 
-    return answer
+        session_key = self.get_session_key(userid, paper_id)
+        self.sessions[session_key] = qa_with_sources
+        return qa_with_sources
 
+    def ask_question(self, question: str, flag: str, userid: str, paper_id: str, prompt: str, index):
+        session_key = self.get_session_key(userid, paper_id)
+        if flag == "first" or session_key not in self.sessions:
+            qa_with_sources = self.create_session(userid, paper_id, index, OpenAIUtils.get_embeddings_api(), prompt)
+            print("First Query")
+        else:
+            qa_with_sources = self.get_session(userid, paper_id)
+            print("Subsequent Query")
 
+        answer = qa_with_sources.run(question)
+        return answer
