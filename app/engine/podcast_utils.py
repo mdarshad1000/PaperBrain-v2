78d315ac2a5148a1c9bc31b098907ced9fff50c3
Mohammad Arshad <mdarshad1000@gmail.com>
2024-06-15 02:10:05 +0530

    failover Gemini-pro to Gemini-flash

--- app/engine/podcast_utils.py
+++ app/engine/podcast_utils.py
@@ -11,9 +11,10 @@ from app.service.openai_service import OpenAIUtils
 from config import (
     GEMINI_SYSTEM_INSTRUCTION,
     GEMINI_USER_INSTRUCTION,
-    GPT_SYSTEM_PROMPT, 
-    GPT_USER_PROMPT
+    GPT_SYSTEM_PROMPT,
+    GPT_USER_PROMPT,
 )
+
 
 def get_podcast_thumbnail(paperurl: str):
     resp = requests.get(paperurl, stream=True)
@@ -29,9 +30,10 @@ def get_podcast_thumbnail(paperurl: str):
 
     return image_bytes
 
+
 def generate_key_insights(research_paper_text: str):
-    genai.configure(api_key=os.getenv('GEMINI_API_KEY_2'))
-    logging.info('Generating key insights using GEMINI')
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY_2"))
+    logging.info("Generating key insights using GEMINI")
     # Set up the model
     generation_config = {
         "temperature": 1,
@@ -40,35 +42,44 @@ def generate_key_insights(research_paper_text: str):
         "max_output_tokens": 8192,
     }
     safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        }
+        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
     ]
 
     system_instruction = GEMINI_SYSTEM_INSTRUCTION
 
-    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
-                                  generation_config=generation_config,
-                                  system_instruction=system_instruction,
-                                  safety_settings=safety_settings)
-
-    convo = model.start_chat(history=[
-    ])
-
     retry_count = 0
-    while retry_count <= 8:
+    while retry_count <= 5:
         try:
-            logging.info('Attempt %d to send message', retry_count + 1)
+            model = genai.GenerativeModel(
+                model_name="gemini-1.5-pro-latest",
+                generation_config=generation_config,
+                system_instruction=system_instruction,
+                safety_settings=safety_settings,
+            )
+            convo = model.start_chat(history=[])
             convo.send_message(
-                GEMINI_USER_INSTRUCTION.format(
-                research_paper_text=research_paper_text))
-            logging.info('Finished generating key insights')
+                GEMINI_USER_INSTRUCTION.format(research_paper_text=research_paper_text)
+            )
+            logging.info("Finished generating key insights")
             return convo.last.text
         except Exception as e:
             logging.error("Error while sending message: %s", e)
-            time.sleep(2 ** retry_count)
-        retry_count += 1
+            time.sleep(2**retry_count)  # Exponential backoff
+            retry_count += 1
+
+    # If all retries failed, try with a different model
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+        system_instruction=system_instruction,
+        safety_settings=safety_settings,
+    )
+    convo = model.start_chat(history=[])
+    convo.send_message(
+        GEMINI_USER_INSTRUCTION.format(research_paper_text=research_paper_text)
+    )
+    logging.info("Finished generating key insights")
+    return convo.last.text
 
 
 def generate_podcast_script(title, abstract, authors, key_findings):
@@ -76,13 +87,15 @@ def generate_podcast_script(title, abstract, authors, key_findings):
     client = OpenAIUtils.get_openai_client()
 
     # Format the input text
-    user_prompt = "\n".join([
-        GPT_USER_PROMPT,
-        f"Title: {title}",
-        f"Abstract: {abstract}",
-        f"Authors: {authors}",
-        f"Key Findings: {key_findings}"
-    ])
+    user_prompt = "\n".join(
+        [
+            GPT_USER_PROMPT,
+            f"Title: {title}",
+            f"Abstract: {abstract}",
+            f"Authors: {authors}",
+            f"Key Findings: {key_findings}",
+        ]
+    )
 
     try:
         logging.info("Sending request to OpenAI...")
@@ -91,8 +104,8 @@ def generate_podcast_script(title, abstract, authors, key_findings):
             response_format={"type": "json_object"},
             messages=[
                 {"role": "system", "content": GPT_SYSTEM_PROMPT},
-                {"role": "user", "content": user_prompt}
-            ]
+                {"role": "user", "content": user_prompt},
+            ],
         )
     except Exception as e:
         logging.error("Failed to send request to OpenAI: %s", e)
