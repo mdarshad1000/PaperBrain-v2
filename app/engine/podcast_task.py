9af61c754a246d098ae0a7b73925c6335aebd1e4
Mohammad Arshad <mdarshad1000@gmail.com>
2024-06-24 03:53:27 +0530

    feat: multspeaker and multitone podcasts!

--- app/engine/podcast_task.py
+++ app/engine/podcast_task.py
@@ -2,6 +2,7 @@ import os
 import json
 import shutil
 import logging
+from typing import List
 
 from app.engine.podcast_utils import (
     generate_key_insights,
@@ -41,22 +42,32 @@ pinecone_service = PineconeService(
 )
 
 
-async def create_podcast(paperurl: str, method: str):
+async def create_podcast(userid: str, paperurl: str, method: str, style: str, speakers: List[str], title: str=None, authors: str=None, abstract: str=None):
     '''
     Creates Podcast using specified method + Script Generation from GPT-4 + TTS w Whisper
     '''
-    paper_id = arxiv_manager.id_from_url(paperurl)
+    if len(speakers) == 1:
+        # TODO: Implement single speaker
+        solo_speaker = speakers[0]
+    else:
+        speaker_one = speakers[0].upper()
+        speaker_two = speakers[1].upper()
+        speaker_three = speakers[2].upper()
 
-    try:
-        title, authors, abstract = arxiv_manager.get_metadata(paper_id)
-    except Exception as e:
-        raise e
+    paper_id = arxiv_manager.id_from_url(paperurl)
+    thumbnail_style = paper_id + '-' + style
+    paper_id = (paper_id + '-' + style + '-' + '-'.join(speakers)).replace(" ", "-")
+
 
     if method == 'gemini':
         # convert pdf to text
         research_paper_text = arxiv_manager.get_pdf_txt(paperurl)
-        # generate key insights
-        key_insights = generate_key_insights(research_paper_text=research_paper_text)
+        key_insights = database_action.get_key_insights(paper_id=paper_id.split("-")[0])
+
+        if key_insights is None:
+            logging.info("Generating Key insights as previously not found for paper_id: %s", paper_id)
+            # generate key insights
+            key_insights = generate_key_insights(research_paper_text=research_paper_text)
 
     elif method == 'rag':
         # Check if a namespace exists in Pinecone with this paper ID
@@ -78,23 +89,29 @@ async def create_podcast(paperurl: str, method: str):
             "What are the main LIMITATIONS of this paper?",
             "What are the main APPLICATION of this paper?",
         ]
-        #TODO: Refactor QASessionManager -> Add method for retrieval without memory.
         key_insights = [ask_questions(question=message, paper_id=paper_id, prompt=prompt, index=pinecone_service.index) for message in messages]
         logging.info("Relevant info retrieved and Key Findings computed")
     else:
         raise ValueError("Invalid method specified")
 
+    # Generate the Podcast Script
     response_dict = generate_podcast_script(
         title=title,
         abstract=abstract,
         authors=authors,
-        key_findings=key_insights)
+        key_findings=key_insights,
+        style=style,
+        speaker_one=speaker_one,
+        speaker_two=speaker_two,
+        speaker_three=speaker_three,
+    )
 
     # generate JSON to insert in Database
     response_dict_json = json.dumps(response_dict) 
 
     # Create directory for the podcast
     podcast_dir = f'{PODCAST_STORAGE_PATH}/{paper_id}'
+
     if not os.path.exists(podcast_dir):
         os.makedirs(podcast_dir)
 
@@ -105,7 +122,7 @@ async def create_podcast(paperurl: str, method: str):
     final_audio = append_audio_segments(speech_segments)
 
     # Overlay background music on final audio
-    final_audio_with_bg = overlay_bg_music_on_final_audio(final_audio)
+    final_audio_with_bg = overlay_bg_music_on_final_audio(final_audio, style=style)
     
     # Add outro and intro
     final_audio_w_outro_intro = add_intro_outro(final_audio_with_bg)
@@ -114,10 +131,10 @@ async def create_podcast(paperurl: str, method: str):
     export_audio(final_audio_w_outro_intro, paper_id)
 
     # Get Paper Thumbnail
-    image_bytes = get_podcast_thumbnail(paperurl=paperurl)
+    image_bytes = get_podcast_thumbnail(paperurl=paperurl, style=style)
 
     # Upload thumbnail to AWS S3
-    s3_manager.upload_thumbnail_to_s3(paper_id=paper_id, image_bytes=image_bytes)
+    s3_manager.upload_thumbnail_to_s3(paper_id=thumbnail_style, image_bytes=image_bytes)
 
     # Upload podcast to AWS S3
     s3_manager.upload_mp3_to_s3(paper_id=paper_id)
@@ -126,20 +143,18 @@ async def create_podcast(paperurl: str, method: str):
     shutil.rmtree(podcast_dir)
 
     new_podcast_url = s3_manager.get_url(f'{paper_id}.mp3')['url']
-    thumbnail_url = s3_manager.get_url(f'{paper_id}.png')['url']
+    
+    thumbnail_url = s3_manager.get_url(f'{thumbnail_style}.png')['url']
 
     database_action.update_podcast_information(
             paper_id=paper_id, 
-            title=title,
-            authors=authors, 
-            abstract=abstract,
             transcript=response_dict_json,
             keyinsights=key_insights if method == 'gemini' else 'None',
             s3_url=new_podcast_url,
             thumbnail=thumbnail_url,
             status='SUCCESS'
-        )
-    
+    )
+
     logging.info("Podcast information updated.")
 
     return "DONE"
--- app/engine/podcast_utils.py
+++ app/engine/podcast_utils.py
@@ -5,6 +5,9 @@ import fitz
 import logging
 import requests
 
+from PIL import Image
+from io import BytesIO
+
 import google.generativeai as genai
 
 from app.service.openai_service import OpenAIUtils
@@ -12,24 +15,48 @@ from app.service.openai_service import OpenAIUtils
 from config import (
     GEMINI_SYSTEM_INSTRUCTION,
     GEMINI_USER_INSTRUCTION,
-    GPT_SYSTEM_PROMPT,
-    GPT_USER_PROMPT,
+    GPT_SYSTEM_PROMPT_MULTISPEAKER,
+    GPT_USER_PROMPT_MULTISPEAKER,
+    TECHNICAL_STYLE_SYSTEM,
+    TECHNICAL_STYLE_USER,
+    MODERATE_STYLE_SYSTEM,
+    MODERATE_STYLE_USER,
+    EASY_STYLE_SYSTEM,
+    EASY_STYLE_USER,
+    FUNNY_STYLE_SYSTEM,
+    FUNNY_STYLE_USER,
 )
 
 
-def get_podcast_thumbnail(paperurl: str):
+def get_podcast_thumbnail(paperurl: str, style: str):
+
+    BADGE_PATH = "static/badges/" + style + ".png"
     resp = requests.get(paperurl, stream=True)
     pdf_bytes = resp.content  # Get the bytes of the PDF directly
 
     pdf_document = fitz.open(None, pdf_bytes)  # Open the PDF from bytes
     first_page = pdf_document[0]
     pix = first_page.get_pixmap()
-    image_bytes = pix.tobytes("png")  # Convert the pixmap to PNG bytes
-
+    
     # Close the PDF document
     pdf_document.close()
+    # Open the main image
+    main_image = Image.open(BytesIO(pix.tobytes("png")))  # Convert the pixmap to PNG bytes
 
-    return image_bytes
+    # Get the badge image
+    badge_image = Image.open(BADGE_PATH)
+    badge_image = badge_image.resize((badge_image.width // 6, badge_image.height // 6))
+
+    badge_position = (main_image.width - badge_image.width - 1, 7)
+    # Paste the badge image onto the main image at the top left corner (0, 0)
+    main_image.paste(badge_image, badge_position)
+
+    # Convert the modified image back to bytes
+    img_byte_arr = BytesIO()
+    main_image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    return img_byte_arr
 
 
 def generate_key_insights(research_paper_text: str):
@@ -37,10 +64,10 @@ def generate_key_insights(research_paper_text: str):
     logging.info("Generating key insights using GEMINI")
     # Set up the model
     generation_config = {
-        "temperature": 1,
+        "temperature": 0.6,
         "top_p": 0.95,
         "top_k": 0,
-        "max_output_tokens": 8192,
+        # "max_output_tokens": 8192,
     }
     safety_settings = [
         {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
@@ -48,7 +75,7 @@ def generate_key_insights(research_paper_text: str):
 
     system_instruction = GEMINI_SYSTEM_INSTRUCTION
 
-    models = ["gemini-1.5-pro-latest", "gemini-1.5-flash"]
+    models = ["gemini-1.5-pro-latest", "gemini-1.5-flash-latest"]
 
     for model_name in models:
         retry_count = 0
@@ -68,25 +95,60 @@ def generate_key_insights(research_paper_text: str):
                 )
                 logging.info("Finished generating key insights")
                 key_insight = convo.last.text
-                print("KEY INSIGHTS: ",key_insight)
+                print("KEY INSIGHTS: ", key_insight)
                 return key_insight
             except Exception as e:
                 logging.error("Error while sending message: %s", e)
-                wait_time = 20 if model_name == "gemini-1.5-flash" else 3  # Increase wait time for flash model
+                wait_time = (
+                    40 if model_name == "gemini-1.5-flash" else 3
+                )  # Increase wait time for flash model
                 time.sleep(wait_time)
                 retry_count += 1
 
     logging.error("Failed to generate key insights with all models")
     return None
 
-def generate_podcast_script(title, abstract, authors, key_findings):
+
+def generate_podcast_script(
+    title,
+    abstract,
+    authors,
+    key_findings,
+    style,
+    speaker_one,
+    speaker_two,
+    speaker_three,
+):
+    s_one_first_name = speaker_one.split(" ")[0]
+    s_two_first_name = speaker_two.split(" ")[0]
+    s_three_first_name = speaker_three.split(" ")[0]
+
+    if style == "expert":
+        system_style = TECHNICAL_STYLE_SYSTEM
+        user_style = TECHNICAL_STYLE_USER
+    elif style == "moderate":
+        system_style = MODERATE_STYLE_SYSTEM
+        user_style = MODERATE_STYLE_USER
+    elif style == "easy":
+        system_style = EASY_STYLE_SYSTEM
+        user_style = EASY_STYLE_USER
+    elif style == "funny":
+        system_style = FUNNY_STYLE_SYSTEM
+        user_style = FUNNY_STYLE_USER
+    else:
+        raise ValueError("Invalid style provided")
 
     client = OpenAIUtils.get_openai_client()
 
     # Format the input text
     user_prompt = "\n".join(
         [
-            GPT_USER_PROMPT,
+            GPT_USER_PROMPT_MULTISPEAKER(
+                user_style=user_style,
+                s_one_first_name=s_one_first_name,
+                s_two_first_name=s_two_first_name,
+                s_three_first_name=s_three_first_name,
+            ),
             f"Title: {title}",
             f"Abstract: {abstract}",
             f"Authors: {authors}",
@@ -100,7 +162,18 @@ def generate_podcast_script(title, abstract, authors, key_findings):
             model="gpt-4-1106-preview",
             response_format={"type": "json_object"},
             messages=[
-                {"role": "system", "content": GPT_SYSTEM_PROMPT},
+                {
+                    "role": "system",
+                    "content": GPT_SYSTEM_PROMPT_MULTISPEAKER(
+                        system_style=system_style,
+                        speaker_one=speaker_one,
+                        speaker_two=speaker_two,
+                        speaker_three=speaker_three,
+                        s_one_first_name=s_one_first_name,
+                        s_two_first_name=s_two_first_name,
+                        s_three_first_name=s_three_first_name,
+                    ),
+                },
                 {"role": "user", "content": user_prompt},
             ],
         )
