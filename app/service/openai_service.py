db3691a3282fbe94e22a62d884f92345664a200d
Mohammad Arshad <mdarshad1000@gmail.com>
2025-01-15 07:47:18 +0530

    [feat]: add agentic-turbo support

--- app/service/openai_service.py
+++ app/service/openai_service.py
@@ -3,28 +3,44 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class OpenAIUtils:
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    MODEL = 'text-embedding-ada-002'
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    MODEL = "text-embedding-ada-002"
 
     @staticmethod
     def get_embeddings_api():
         from langchain_openai import OpenAIEmbeddings
-        return OpenAIEmbeddings(model=OpenAIUtils.MODEL, openai_api_key=OpenAIUtils.OPENAI_API_KEY)
+
+        return OpenAIEmbeddings(
+            model=OpenAIUtils.MODEL, openai_api_key=OpenAIUtils.OPENAI_API_KEY
+        )
 
     @staticmethod
-    def get_chat_api(model_name='gpt-4o', temperature=0.0, streaming=True, callbacks=None):
+    def get_chat_api(
+        model_name="gpt-4o", temperature=0.0, streaming=True, callbacks=None
+    ):
         from langchain_openai import ChatOpenAI
+
         return ChatOpenAI(
             openai_api_key=OpenAIUtils.OPENAI_API_KEY,
             model_name=model_name,
             temperature=temperature,
             streaming=streaming,
-            callbacks=callbacks
+            # callbacks=callbacks
         )
 
     @staticmethod
     def get_async_openai_client():
         from openai import AsyncOpenAI
+
         return AsyncOpenAI(api_key=OpenAIUtils.OPENAI_API_KEY)
 
+    @staticmethod
+    def agentic_client():
+        from openai import OpenAI, AsyncOpenAI
+
+        return AsyncOpenAI(
+            base_url=os.getenv("AGENTIC_BASE_URL"),
+            api_key=os.getenv("AGENTIC_API_KEY")
+        )
