d515afd49c5d5eaae0b20b8d0540c3d25d49a47b
Mohammad Arshad <mdarshad1000@gmail.com>
2025-01-15 07:46:08 +0530

    [feat]: Chat using agentic-turbo

--- app/api/routers/chat.py
+++ app/api/routers/chat.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends
 
-from app.engine.chat_utils import create_prompt_template, ask_questions
+from app.engine.chat_utils import create_prompt_template, ask_questions_agentic, ask_questions
 from app.service.pinecone_service import PineconeService
-from config import CHAT_PROMPT_TEMPLATE
+from app.service.openai_service import OpenAIUtils
+from config import CHAT_PROMPT_TEMPLATE, CHAT_PROMPT_TEMPLATE_MANUAL
 
 import os
 
@@ -13,19 +14,35 @@ chat_router = r = APIRouter()
 
 prompt = create_prompt_template(CHAT_PROMPT_TEMPLATE)
 
+
 def get_pinecone_service():
-    api_key = os.getenv('PINECONE_API_KEY_2')
-    index_name = os.getenv('PINECONE_INDEX_NAME_2')
-    environment = os.getenv('PINECONE_ENVIRONMENT_2')
-    return PineconeService(api_key=api_key, index_name=index_name, environment=environment)
-
-@r.post('/explain-new') 
-async def explain_question(paper_id: str, message: str, pinecone_service=Depends(get_pinecone_service)):
-
-    answer = ask_questions(
-        question=message, 
-        paper_id=paper_id,
-        prompt=prompt,
-        index=pinecone_service.index
+    api_key = os.getenv("PINECONE_API_KEY_2")
+    index_name = os.getenv("PINECONE_INDEX_NAME_2")
+    environment = os.getenv("PINECONE_ENVIRONMENT_2")
+    return PineconeService(
+        api_key=api_key, index_name=index_name, environment=environment
     )
+
+def get_async_openai_client():
+    return OpenAIUtils.get_async_openai_client()
+
+def get_agentic_client():
+    return OpenAIUtils.agentic_client()
+
+@r.post("/explain-new")
+async def explain_question(
+    paper_id: str, message: str, pinecone_service=Depends(get_pinecone_service)
+):
+
+    answer = await ask_questions_agentic(
+        question=message,
+        paper_id=paper_id,
+        prompt=CHAT_PROMPT_TEMPLATE_MANUAL,
+        pinecone_service=pinecone_service,
+        async_openai_client=get_async_openai_client(),
+        agentic_client=get_agentic_client(),
+    )
+    # answer = await ask_questions(
+    #     question=message, paper_id=paper_id, prompt=prompt, index=pinecone_service.index
+    # )
     return {"answer": answer}
--- app/engine/chat_utils.py
+++ app/engine/chat_utils.py
@@ -16,46 +16,46 @@ from app.service.arxiv_service import ArxivManager
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def create_prompt_template(template_str):
     return PromptTemplate.from_template(template=template_str)
 
 
-def prepare_data(paperurl: str):
-    '''
+async def prepare_data(paperurl: str):
+    """
     Split PDF into Chunks
     Prepare Metadata
-    '''
+    """
     paper_id = ArxivManager.id_from_url(paperurl=paperurl)
-    pdf_txt = ArxivManager.get_pdf_txt(paperurl=paperurl)
-    paper_metadata = ArxivManager.get_metadata(paper_id=paper_id)
+    pdf_txt = await ArxivManager.get_pdf_txt(paperurl=paperurl)
+
+    paper_metadata = await ArxivManager.get_metadata(paper_id=paper_id)
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=120,
     )
     pages = text_splitter.split_text(pdf_txt)
-    
+
     # Initialize Data structures for for data prep
-    texts = [] 
-    metadatas = [] 
+    texts = []
+    metadatas = []
 
     # Adding text chunks and preparing Metadata
     for item, page in enumerate(pages):
         texts.append(page)
         metadata = {
-            'paper_id': paper_id,
-            'source': page,
-            'page_no': item + 1,
-            'title': paper_metadata[0],
-            'authors': paper_metadata[1]
+            "paper_id": paper_id,
+            "source": page,
+            "page_no": item + 1,
+            "title": paper_metadata[0],
+            "authors": paper_metadata[1],
         }
-        record_metadata = {
-            "chunk_id": item, "text": texts[item], **metadata
-        }
+        record_metadata = {"chunk_id": item, "text": texts[item], **metadata}
         metadatas.append(record_metadata)
 
     logging.info(f"Completed splitting PDF for paper_id: {paperurl}")
@@ -74,25 +74,24 @@ def chunker(iterable, batch_size=100):
 
 
 async def embed_and_upsert(texts: List[str], metadatas: List[str], index) -> str:
-    '''
+    """
     Creaate Embeddings and Upsert to Pinecone
-    '''
+    """
     # Create an embedding object
     embed = OpenAIUtils.get_embeddings_api()
 
     # Create pinecone.Index with pool_threads=30 (limits to 30 simultaneous requests)
     ids = [str(uuid4()) for _ in range(len(texts))]
-    import timeit
-    # time here
-    print('upsert started', timeit.default_timer())
-    # Send upsert requests in parallel
-    async_results =  [
-        index.upsert(vectors=zip(ids_chunk, embeds_chunk,
-                     metadatas_chunk), async_req=True)
-        for ids_chunk, embeds_chunk, metadatas_chunk in zip(chunker(ids), chunker(embed.embed_documents(texts)), chunker(metadatas))
-    ]
 
-    print('upsert ended', timeit.default_timer())
+    # Send upsert requests in parallel
+    async_results = [
+        index.upsert(
+            vectors=zip(ids_chunk, embeds_chunk, metadatas_chunk), async_req=True
+        )
+        for ids_chunk, embeds_chunk, metadatas_chunk in zip(
+            chunker(ids), chunker(embed.embed_documents(texts)), chunker(metadatas)
+        )
+    ]
 
     # Wait for and retrieve responses (this raises in case of error)
     [async_result.get() for async_result in async_results]
@@ -100,21 +99,72 @@ async def embed_and_upsert(texts: List[str], metadatas: List[str], index) -> str
     return "Embedded the Arxiv Paper"
 
 
-def ask_questions(question: str, paper_id: int, prompt: str, index):
-    '''
-    Retrieve Context and Generate Answers
-    '''
+async def ask_questions_agentic(
+    question: str,
+    paper_id: int,
+    prompt: str,
+    pinecone_service,
+    async_openai_client,
+    agentic_client,
+):
+    """
+    Do Retrieval & Generation without using LangChain
+    """
+    top_k = 7
 
+    response = await async_openai_client.embeddings.create(
+        input=question, model="text-embedding-ada-002"
+    )
+
+    query_vector = response.data[0].embedding
+    retrieved_chunks = await pinecone_service.retrieval(
+        vector=query_vector, k=top_k, filter_params={"paper_id": {"$eq": paper_id}}
+    )
+
+    formatted_chunks = "\n".join(
+        [
+            f"<<<\nChunk: {i+1}\nText: \"{match['metadata']['text']}\"\n>>>"
+            for i, match in enumerate(retrieved_chunks["matches"])
+        ]
+    )
+    print(formatted_chunks)
+    # Generate Response from OpenAI
+    answer = await agentic_client.chat.completions.create(
+        model="agentic-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": """You are an expert assistant for question-answering tasks. 
+                    You will be provided with a Question and some paragraph (Context) from a Research Paper. 
+                    Your task is to answer the question based on the given context""",
+            },
+            {
+                "role": "user",
+                "content": prompt.format(context=formatted_chunks, question=question),
+            },
+        ],
+    )
+    
+    return answer
+    return answer.choices[0].message.content
+
+
+async def ask_questions(question: str, paper_id: int, prompt: str, index):
+    """
+    Retrieve Context and Generate Answers using LangChain
+    """
     text_field = "text"
     # Create an embedding object
     embeddings = OpenAIUtils.get_embeddings_api()
 
     vectorstore = PineconeVectorStore(
-        index=index, embedding=embeddings, text_key=text_field,
+        index=index,
+        embedding=embeddings,
+        text_key=text_field,
     )
 
     llm = OpenAIUtils.get_chat_api(
-        model_name='gpt-3.5-turbo-16k',
+        model_name="gpt-3.5-turbo-16k",
         temperature=0.0,
         streaming=True,
         callbacks=[StreamingStdOutCallbackHandler()],
@@ -124,15 +174,12 @@ def ask_questions(question: str, paper_id: int, prompt: str, index):
         llm=llm,
         chain_type="stuff",
         retriever=vectorstore.as_retriever(
-            search_kwargs={
-                'k': 4,
-                'filter': {'paper_id':{"$eq":paper_id}}
-                }),
+            search_kwargs={"k": 4, "filter": {"paper_id": {"$eq": paper_id}}}
+        ),
         chain_type_kwargs={
             "prompt": prompt,
             "verbose": True,
-        }
+        },
     )
     answer = qa_with_sources.run(question)
-
     return answer
